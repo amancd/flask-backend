7854495a984ec22c5a9eaf40e26771fa7edee9c5
amancd <imamanwp@gmail.com>
2025-05-05 04:07:29 +0530

    added url

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from models import RecommendationRequest, RecommendationResponse, Assessment
 from recommender import recommend
@@ -8,7 +10,7 @@ app = FastAPI()
 # Allow requests from your frontend (adjust the origin as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # React dev server
+    allow_origins=["https://shl-recommender-app.vercel.app"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,3 +24,8 @@ def health_check():
 def recommend_assessments(payload: RecommendationRequest):
     results = recommend(payload.job_description)
     return {"recommended_assessments": results}
+
+# 👇 Optional for local testing
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
